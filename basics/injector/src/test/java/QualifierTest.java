cf7eb9b3d537bcd090c0ea6ff316bc6743d6c340
Zhangzhe Shen <zzshen@thoughtworks.com>
2016-11-28 01:55:06 +0800

    add inherited test case

--- basics/injector/src/test/java/QualifierTest.java
+++ basics/injector/src/test/java/QualifierTest.java
@@ -1,6 +1,6 @@
 import com.sid.injector.Container;
-import resources.qualifier.*;
 import org.junit.Test;
+import resources.qualifier.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -9,7 +9,8 @@ public class QualifierTest {
     @Test
     public void should_inject_bind_qualifier_annotated_field() {
         Container container = new Container();
-        container.register(LeatherBook.class).qualifier(Leather.class).toInstance(Color.TAN);
+        container.register(LeatherBook.class)
+                .qualifier(Leather.class).toInstance(Color.TAN);
 
         LeatherBook book = container.resolve(LeatherBook.class);
 
@@ -19,11 +20,25 @@ public class QualifierTest {
     @Test
     public void should_inject_bind_qualifier() {
         Container container = new Container();
-        container.register(PaperBook.class).qualifier(Paper.class).toInstance(Color.WHITE);
+        container.register(PaperBook.class)
+                .qualifier(Paper.class).toInstance(Color.WHITE);
 
         PaperBook book = container.resolve(PaperBook.class);
 
         assertThat(book.getColor(), is(Color.WHITE));
+    }
+
+    @Test
+    public void should_inject_bind_qualifier_inherited() {
+        Container container = new Container();
+        container.register(PaperBook.class)
+                .qualifier(Paper.class).toInstance(Color.WHITE)
+                .bind(String.class).annotatedWith("name").toInstance("Harry Porter");
+
+        PaperBook book = container.resolve(PaperBook.class);
+
+        assertThat(book.getColor(), is(Color.WHITE));
+        assertThat(book.getName(), is("Harry Porter"));
     }
 }
 
