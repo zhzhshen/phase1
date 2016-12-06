63433219da2b9510e3fc8e75ef19e1f3e4e2145e
Zhangzhe Shen <zzshen@thoughtworks.com>
2016-12-06 11:57:14 +0800

    listen to mouseup to change height of textarea

--- ui/editor/editor.js
+++ ui/editor/editor.js
@@ -15,44 +15,47 @@ var view = function () {
     undraggableRow();
 
     $('.component').each(function(){
-        let widget = $(this).find('.form-control');
-        let height = widget.outerHeight();
-        $(this).attr("data-height", height);
-
-        let children = $(this).children();
-        children.each(function(component){
+        $(this).children().each(function(component){
             $(this).remove();
         });
 
         if ($(this).hasClass("title")) {
             $("<h4>"+ $(this).attr("data-content") +"</h4>").appendTo($(this));
         } else if ($(this).hasClass("textarea")) {
-            $("<p style=\"height: "+ height + "px\">"+ $(this).attr("data-content") +"</p>").appendTo($(this));
+            $("<p style=\"height: "+ $(this).attr("data-height") + "px\">"+ $(this).attr("data-content") +"</p>").appendTo($(this));
         }
     });
 }
 
 var edit = function () {
-    clearContent();
     draggableRow();
     $('.component').each(function() {
+        $(this).children().each(function(component){
+            $(this).remove();
+        });
+
         let component = $(this);
         $("<span class=\"glyphicon glyphicon-move\"></span>").appendTo(component);
+
         if ($(this).hasClass('title')) {
             $("<input class=\"form-control\" type=\"text\" value=\"" + component.attr("data-content") + "\">")
-                .on('change', function() {
+                .change(function() {
                     component.attr("data-content", $(this).val());
                 })
                 .appendTo(component);
         } else if ($(this).hasClass('textarea')) {
             $("<textarea class=\"form-control\" style=\"height: "+ component.attr("data-height") + "px\">" + component.attr("data-content") + "</textarea>")
-                .on('change', function() {
+                .mouseup(function() {
+                    component.attr("data-height", $(this).outerHeight());
+                })
+                .change(function() {
                     component.attr("data-content", $(this).val());
                 })
                 .appendTo(component);
         }
+
         $("<span class=\"glyphicon glyphicon-remove-circle\"></span>")
-            .on('click', function() {
+            .click(function() {
                 $(this).closest('.row').remove();
             })
             .appendTo(component);
@@ -74,14 +77,5 @@ var undraggableRow = function() {
 var wrapColumn = function() {
     $('li.column').each(function() {
         $(this).replaceWith('<div class="panel panel-default col-md-6"><div class="panel-body">' + this.innerHTML + '</div></div>');
-    });
-}
-
-var clearContent = function () {
-    $('ul.row').each(function(components){
-        let children = $(this).children();
-        children.each(function(component){
-            $(this).empty();
-        });
     });
 }
