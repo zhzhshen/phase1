ad7857a2d2608a5cd6b62ffde5143e5d0bd643f5
Zhangzhe Shen <zzshen@thoughtworks.com>
2016-12-15 23:24:36 +0800

    simple create article test

--- api/ruby-demo/test/models/article_test.rb
+++ api/ruby-demo/test/models/article_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save article without title" do
+    article = Article.new
+    assert_not article.save, "Saved the article without a title"
+  end
+
+  test "should save article with title" do
+    article = Article.new({:title => "title"})
+    assert article.save, "Saved the article with a title"
+    assert_equal article.title, "title"
+  end
 end
