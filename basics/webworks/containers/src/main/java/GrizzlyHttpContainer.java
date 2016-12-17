d0f10c7f9a6349dce2283fa90fa1a934dd040016
Zhangzhe Shen <zzshen@thoughtworks.com>
2016-12-17 17:35:15 +0800

    add hk2 for injection

--- basics/webworks/containers/src/main/java/GrizzlyHttpContainer.java
+++ basics/webworks/containers/src/main/java/GrizzlyHttpContainer.java
@@ -1,16 +1,24 @@
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Response;
+import org.glassfish.hk2.api.DynamicConfiguration;
+import org.glassfish.hk2.api.DynamicConfigurationService;
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.api.ServiceLocatorFactory;
+import org.glassfish.hk2.utilities.BuilderHelper;
 
 import javax.ws.rs.Path;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class GrizzlyHttpContainer extends HttpHandler {
+    private ServiceLocator locator = ServiceLocatorFactory.getInstance().create(null);
+
     private ResourceConfig resourceConfig;
 
     public GrizzlyHttpContainer(ResourceConfig resourceConfig) {
         this.resourceConfig = resourceConfig;
+        populate(locator, resourceConfig);
     }
 
     @Override
@@ -20,7 +28,7 @@ public class GrizzlyHttpContainer extends HttpHandler {
                 .filter(resource -> ((Path) resource.getAnnotation(Path.class)).value().equals(request.getHttpHandlerPath()))
                 .findFirst().map(resource -> resource).get();
 
-        response.getWriter().write((String) findMethod(resourceClass, request.getMethod()).invoke(resourceClass.newInstance()));
+        response.getWriter().write((String) findMethod(resourceClass, request.getMethod()).invoke(locator.getService(resourceClass)));
     }
 
     private Method findMethod(Class resourceClass, org.glassfish.grizzly.http.Method method) {
@@ -29,5 +37,15 @@ public class GrizzlyHttpContainer extends HttpHandler {
 
     private boolean methodMatched(Method m, org.glassfish.grizzly.http.Method method) {
         return Arrays.stream(m.getAnnotations()).filter(annotation -> annotation.toString().contains(method.getMethodString())).count() == 1;
+    }
+
+    private void populate(ServiceLocator locator, ResourceConfig resourceConfig) {
+        DynamicConfigurationService dcs = locator.getService(DynamicConfigurationService.class);
+        DynamicConfiguration config = dcs.createDynamicConfiguration();
+
+        resourceConfig.getResources()
+                .stream()
+                .forEach(resourceClass -> config.bind(BuilderHelper.createDescriptorFromClass(resourceClass)));
+        config.commit();
     }
 }
