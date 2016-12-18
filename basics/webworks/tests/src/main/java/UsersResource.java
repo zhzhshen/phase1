34fdfdc6392346564d91500dd872d9dd54576fd3
Zhangzhe Shen <zzshen@thoughtworks.com>
2016-12-18 17:55:48 +0800

    users resource return list of users

--- basics/webworks/tests/src/main/java/UsersResource.java
+++ basics/webworks/tests/src/main/java/UsersResource.java
@@ -4,12 +4,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Arrays;
+import java.util.List;
 
 @Path("/users")
 public class UsersResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public User get() {
-        return new User("Sid", "Shen");
+    public List<User> get() {
+        return Arrays.asList(new User("Sid", "Shen"));
     }
 }
--- basics/webworks/tests/src/test/java/UsersResourceTest.java
+++ basics/webworks/tests/src/test/java/UsersResourceTest.java
@@ -1,4 +1,3 @@
-import models.User;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
 import org.junit.Test;
@@ -8,6 +7,8 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
 import java.net.URI;
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -29,10 +30,10 @@ public class UsersResourceTest {
         Client client = ClientBuilder.newClient().register(MyMessageBodyReader.class);
         Response res = client.target(BASE_URI + "users").request("application/json").get();
 
-        User user = res.readEntity(User.class);
+        Map user = (Map) res.readEntity(List.class).get(0);
         assertThat(res.getStatus(), is(200));
-        assertThat(user.getFirstName(), is("Sid"));
-        assertThat(user.getLastName(), is("Shen"));
+        assertThat(user.get("firstName"), is("Sid"));
+        assertThat(user.get("lastName"), is("Shen"));
     }
 
 }
