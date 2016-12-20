228192435dc400ca7614bd46e2735cb37273e0eb
Zhangzhe Shen <zzshen@thoughtworks.com>
2016-12-20 21:53:52 +0800

    reorganize test code structure

--- basics/orm/src/test/java/SingleTableMappingTest.java
+++ basics/orm/src/test/java/SingleTableMappingTest.java
@@ -11,25 +11,39 @@ import java.sql.SQLException;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.core.IsNull.nullValue;
 
 public class SingleTableMappingTest {
     private final String BASE_URL = "jdbc:mysql://localhost:3306";
     private final String DB_NAME = "orm_test";
     private final String USER_NAME = "mysql";
     private final String PASSWORD = "mysql";
+    private String EXISTING_ID = "1";
+    private String INEXIST_ID = "2";
     private MigrationManager migrationManager;
+    private FinderUtil finderUtil;
+    private ConnectionConfig connectionConfig;
 
     @Before
     public void before() {
-        migrationManager = new MigrationManager(new ConnectionConfig(BASE_URL, DB_NAME, USER_NAME, PASSWORD));
+        connectionConfig = new ConnectionConfig(BASE_URL, DB_NAME, USER_NAME, PASSWORD);
+        migrationManager = new MigrationManager(connectionConfig);
+        finderUtil = new FinderUtil(new ConnectionConfig(BASE_URL, DB_NAME, USER_NAME, PASSWORD));
+
         migrationManager.migrate();
     }
 
     @Test
+    public void should_fail_to_find_by_inexist_id () throws SQLException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+        User user = finderUtil.get(User.class, INEXIST_ID);
+        assertThat(user, nullValue());
+    }
+
+    @Test
     public void should_success_to_find_by_id () throws SQLException, InstantiationException, IllegalAccessException, NoSuchFieldException {
-        User user = new FinderUtil(new ConnectionConfig(BASE_URL, DB_NAME, USER_NAME, PASSWORD)).get(User.class, "1");
+        User user = finderUtil.get(User.class, EXISTING_ID);
         assertThat(user, notNullValue());
-        assertThat(user.userId, is("1"));
+        assertThat(user.userId, is(EXISTING_ID));
         assertThat(user.firstName, is("Sid"));
         assertThat(user.lastName, is("Shen"));
         assertThat(user.age, is(24));
